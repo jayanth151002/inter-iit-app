33ee9f3944e034adb7d9566b0ef9c898ede92405
Jayanth <jayanth151002@gmail.com>
2022-02-20 20:27:57 +0530

    Login form

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,36 @@
+import { useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
 
-    axios.get('/trial')
-        .then((res) => console.log(res.data))
-        .catch((res) => console.log(res))
+    const [rollno, setRollno] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const data = new FormData()
+        data.append('rollno', rollno)
+        data.append('password', password)
+        console.log(rollno, password)
+        axios.post('/login', { rollno: rollno, password: password })
+            .then(res => console.log(res.data))
+            .catch(res => console.log(res))
+    }
 
     return (
         <div>
             Home
-            
+            <form>
+                <div>
+                    <label for="rollno">Roll Number</label>
+                    <input type="text" onChange={(e) => setRollno(e.target.value)} />
+                </div>
+                <div>
+                    <label for="rollno">Password</label>
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button onClick={handleSubmit}>Submit</button>
+            </form>
         </div>
     )
 }
--- server/router/auth.js
+++ server/router/auth.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
 app.use(express.json());
+app.use(router);
+
 
 router.get('/trial', async (req, res) => {
     console.log("In server")
     res.status(200).send("Connected to server")
+})
+
+router.post('/login', async (req, res) => {
+    console.log(req.body)
+    res.status(200).send("Logged In")
 })
 
 
